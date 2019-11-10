1b4fe516f2a90c418bc0a445cb250b6d03b1517f
Patrick Dupuis <p.dupuis@live.com>
2019-11-10 16:35:10 -0500

    don't install CommonTests Quarks

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # the two included paths should be placed last (see: https://stackoverflow.com/a/20167038)
-qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks $TRAVIS_BUILD_DIR/testsuite/classlibrary
+# qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks $TRAVIS_BUILD_DIR/testsuite/classlibrary
+QPM_DEBUG=1 qpm test.run -l '/home/travis/build/supercollider/supercollider/BUILD/travis_test_run.json' --path $SCLANG --include $HOME/Quarks $TRAVIS_BUILD_DIR/testsuite/classlibrary
