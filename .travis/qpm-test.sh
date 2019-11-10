823d32f9a2e0dcf6709bcaaf6541a7b5ac180536
Patrick Dupuis <p.dupuis@live.com>
2019-11-10 16:34:30 -0500

    incl repo testsuite in qpm test.run

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks
+# the two included paths should be placed last (see: https://stackoverflow.com/a/20167038)
+qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks $TRAVIS_BUILD_DIR/testsuite/classlibrary
